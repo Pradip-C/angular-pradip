7ee9df3d51b7ae0f95efa8e5413e1d1a06533ea9
Pradip-C <125476725+Pradip-C@users.noreply.github.com>
2023-02-15 20:43:43 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector:'my-app',
-  templateUrl:'./app.component.html',
-  styleUrls:['./app.component.scss']
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent{
-  
+export class AppComponent {
+  name = 'Pradip Chavan';
+  value = '';
+
+  onClick() {
+    this.value = 'Umesh';
+  }
+  onClear() {
+    this.value = '';
+  }
 }
